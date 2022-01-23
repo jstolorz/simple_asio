6e1c95cb5040ba31e9920ecfca4b129b9d4b2105
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-01-23 15:17:23 +0100

    adding chat server example

--- client/client.h
+++ client/client.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <thread>
+#include <asio.hpp>
+#include "../common/chat_message.h"
+
--- server/main.cpp
+++ server/main.cpp
@@ -3,7 +3,19 @@
 
 int main(){
 
-    chat_message m;
+    try{
+
+        asio::io_context io_context;
+
+        tcp::endpoint endpoint(tcp::v4(), 90000);
+
+        chat_server s(io_context,endpoint);
+
+        io_context.run();
+
+    }catch (std::exception& e){
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
 
     return 0;
 }
