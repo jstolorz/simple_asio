ff75d24f53817d2c68630e10b8551e22a50d0e60
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-01-23 15:46:41 +0100

    adding part of chat client example

--- client/client.h
+++ client/client.h
@@ -7,3 +7,18 @@
 #include <asio.hpp>
 #include "../common/chat_message.h"
 
+using asio::ip::tcp;
+
+typedef std::deque<chat_message> chat_message_queue;
+
+class chat_client{
+
+
+
+private:
+    asio::io_context& io_context_;
+    tcp::socket socket_;
+    chat_message read_msg_;
+    chat_message_queue write_msgs_;
+};
+
